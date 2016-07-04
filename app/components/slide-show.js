3c72cc10cb8667f2148e0f4f608608ff2461f549
NathanielScottStevens <nathaniel.scott.stevens@gmail.com>
2016-07-04 11:38:14 -0400

    Slide-show now using css for transition.

--- app/components/slide-show.js
+++ app/components/slide-show.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   index: 0,
-  transitionInterval: 16,
   pauseInterval: 3000,
   pixelDist: 15,
   transitionUpdate: null,
@@ -13,56 +12,36 @@ export default Ember.Component.extend({
   didInsertElement() {
     this.images = $('.slide');
 
-    $.each(this.images, function(index, element) {
-      if (index !== 0) {
-        $(element).css('left', '980px');
-      }
-    });
-
-    let that = this;
-    this.transitionUpdate = setInterval(function() { that.beginTransition(); }, this.pauseInterval);
-  },
-
-  beginTransition: function(){
-    let that = this;
-    let leftImg = this.images[this.index];
-    let rightImg;
-
-    if (this.index === this.images.length - 1){
-      rightImg = this.images[0];
-    }
-    else {
-      rightImg = this.images[this.index + 1];
-    }
 
     $.each(this.images, function(index, element) {
-        if (element !== leftImg && element !== rightImg) {
-          $(element).css('display', 'none');
+        if (index === 0) {
+          $(element).addClass('slide-on');
         }
         else {
-          $(element).css('display', 'block');
+          $(element).addClass('slide-waiting');
         }
-    });
+      }
+    );
 
-    $(rightImg).css('left', '980px');
+    let that = this;
 
-    this.update = setInterval(function() { that.updateSlides(leftImg, rightImg); }, this.transitionInterval);
+    this.transitionUpdate = setInterval(function() { that.step(); }, this.pauseInterval);
   },
 
-  updateSlides: function(left, right){
-    this.moveImage(left);
-    this.moveImage(right);
+  step(){
+    var rightIndex = (this.index + 1) % this.images.length;
+    var waitIndex = (this.index + 2) % this.images.length;
 
-    if ($(left).position().left < -980){
-      clearInterval(this.update);
-      this.index = (this.index + 1) % this.images.length;
-      $(left).css('left', '-980px');
-    }
-  },
+    var left = this.images[this.index];
+    var right = this.images[rightIndex];
+    var wait = this.images[waitIndex];
 
-  moveImage: function(image){
-    let position = $(image).position();
-    $(image).css('left', (position.left - this.pixelDist).toString() + 'px');
+    $(left).attr('class', 'slide slide-off');
+    $(right).attr('class', 'slide slide-on');
+    $(wait).attr('class', 'slide slide-waiting');
+
+
+    this.index = (this.index + 1) % this.images.length;
   },
 
 });
